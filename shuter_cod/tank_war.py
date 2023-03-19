0a01d3a2cb321989b4c4dd3bbce8e75e1bd785e9
Мистер Гномик <128287367+SaidSelim2000@users.noreply.github.com>
2023-03-19 12:59:49 +0300

    Add files via upload
    Создал новую игру "Война Мемов", или же "Mems War"
    Мутированный Майк Лозовский защищает страну Мемов которую пытаются захватить армия ШРЕКОВ
    Со Временем проект изменю, если вам что-то не понравится.
    Издатель: Сайд Селим

---
+++ shuter_cod/tank_war.py
@@ -0,0 +1,152 @@
+from pygame import *
+from random import randint
+
+mixer.init()
+mixer.music.load('music1.mp3')
+mixer.music.play()
+fire_sound = mixer.Sound('odnokratnyiy-piu.mp3')
+
+font.init()
+font1 = font.SysFont('Arial Black', 80) #
+win = font1.render('Танк выиграл!', True, (255, 15, 3)) #
+lose = font1.render('Спецназ выиграл!', True, (180, 15, 3)) #
+
+font2 = font.SysFont(None, 50)
+
+img_back = "backround7.jpg"
+img_hero = "tank1.jpg"
+img_bullet = "bullet.png"
+img_enemy = "specnaz.jpg" 
+
+score = 0
+goal = 100
+lost = 0 
+max_lost = 3 #
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.x - 5:
+            self.rect.x -= self.speed
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.y < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_SPACE]:
+            self.fire()
+            fire_sound.play()
+
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx-7, self.rect.top, 15, 20, 15)
+        bullets.add(bullet) #
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 50)
+            self.rect.y = 6
+            lost = lost + 1
+
+class Bullet(GameSprite): #
+    def update(self): #
+        self.rect.y -= self.speed #
+        if self.rect.y < -40: #
+            self.kill() #
+
+win_width = 800
+win_height = 800
+display.set_caption('Танковая Война')
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 30)
+
+monsters = sprite.Group()
+for i in range(1, 12):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
+
+bullets = sprite.Group() #
+
+finish = True
+run = True
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN: #
+            if e.key == K_SPACE: #
+                fire_sound.play() #
+                ship.fire() #
+
+    if finish:
+        window.blit(background,(0,0))
+
+        text = font2.render("Танк(Ты):" + str(score), 1, (0, 210, 0))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Враги:" + str(lost), 1, (234, 12, 0))
+        window.blit(text_lose, (10, 50))
+
+        ship.update()
+        monsters.update()
+        bullets.update() 
+
+        ship.reset()
+        monsters.draw(window)
+        bullets.draw(window) #
+
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for i in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(3, 10))
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = False
+            window.blit(lose, (50, 50))
+
+        if score >= goal:
+            finish = False
+            window.blit(win, (50, 50))
+
+    else:
+        finish = True
+        score = 0
+        lost = 0
+        for b in bullets:
+            b.kill()
+        for m in monsters:
+            m.kill()
+
+        time.delay(1500)
+        for i in range(2, 10):
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 40, 20, randint(1, 5))
+            monsters.add(monster)
+
+    display.update()
+    time.delay(50)
+
+# Изменить картинки
+# Изменить размер окня
+# Изменить управление игроком
+# Изменить музыку
